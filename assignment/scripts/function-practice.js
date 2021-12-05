b16e5ae7d5fa7dc9e6c08c0a81a31a54b0199908
bryant.nupdal <nupdalbryant@icloud.com>
2021-12-05 01:00:05 -0600

    completed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -54,28 +54,26 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 //    array is empty, return `undefined`.
 function getLast( array ) {
 array = [0,19,82,34,4,6,67,28,4];
-let the_last_item = array.length;
+let the_last_item = array[array.length-1];
 return the_last_item;
 }
-console.log( ' the last # in the array is ', getLast());
+console.log( ' the last # in the array is # ', getLast());
 
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find( value, array ){
-  array = [0,19,82,34,4,6,67,28,4];
-  console.log('the values in array are: ', array );
-  for (value of array ){
-
-    if (value = array){
+  array = ['0','19','82','34','4','6','67','28','4'];
+  for (value in array){
+    if (array[value] = '34'){
       return true;
     }
-    else {
+    else{
       return false;
     }
-    }
-    }
-console.log('can the value be found, (true/false)? ', find());
+  }
+}
+console.log('Was the value found? true/false: ', find());
 
 // ----------------------
 // Stretch Goals
@@ -83,7 +81,12 @@ console.log('can the value be found, (true/false)? ', find());
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === 'a'){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
@@ -91,16 +94,36 @@ console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
   let sum = 0
+  let numberArray = [3,5,10,19,11,-6,-2,8,-4,92];
+  for (let i=0; i < numberArray.length; i++){
+  let currentNumber = numberArray[i];
+  sum = sum + currentNumber;
+}
   // TODO: loop to add items
   return sum;
 }
+console.log('the total sum is ', sumAll());
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
+let arr = [3,5,10,19,11,-6,-2,8,-4,92];
+function getPositive(array){
+  return array.filter(function(value){
+    return value > 0;
+  })
+}
+console.log(' The new positve array is ', getPositive(arr));
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
 //     here in a comment, write the function, and test it!
+
+//Create a function that takes the age in years and returns the age in days.
+function calcAge(years){
+  const days = 365;
+    let age = years * days;
+    return age;
+}
+console.log('If you are 24 years old, in days that would be: ', calcAge(24));
